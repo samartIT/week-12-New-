16d00fdac369169ca581af46cd5719fad0a4f527
PeetTharathanakul <peet12131424@gmail.com>
2019-04-03 11:22:35 +0700

    Test

---
+++ week12(New)/Assets/Scripts/Fireball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 10f;
+    int damage = 1;
+    void Update()
+    {
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        PlayerCharactor player = other.GetComponent<PlayerCharactor>();
+        if (player != null)
+        {
+            Debug.Log("Player hit");
+        }
+        Destroy(gameObject);
+    }
+}
