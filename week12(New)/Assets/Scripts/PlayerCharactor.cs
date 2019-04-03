e36de0ce6a80921840eae786e0498417e4247067
PeetTharathanakul <peet12131424@gmail.com>
2019-04-03 13:29:59 +0700

    enemy shoot

---
+++ week12(New)/Assets/Scripts/PlayerCharactor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharactor : MonoBehaviour
+{
+    private int _health;
+    void Start()
+    {
+        _health = 5;
+    }
+
+    // Update is called once per frame
+    public void Hurt(int damage)
+    {
+        _health -= damage;
+        Debug.Log("Health" + _health);
+    }
+}
