c4d165b6565fdf7193ab53a96189f841a04aa136
Thatsarat <59070060@it.kmitl.ac.th>
2019-04-03 13:12:31 +0700

    week-12

---
+++ week12(New)/Assets/_59070060/Scripts/Fireball.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float speed = 10f;
+    int damage = 1;
+    void Start()
+    {
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        PlayerCharactor player = other.GetComponent<PlayerCharactor>();
+        if (player != null)
+        {
+            //Debug.Log("Plaer hit");
+            player.Hurt(damage);
+        }
+        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        
+    }
+}
