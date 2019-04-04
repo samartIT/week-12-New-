4ef2ecad1afbf6484fbfb8f560aa54cf327a68b2
Chanikarn <59070034@kmitl.ac.th>
2019-04-04 20:58:11 +0700

    Week 12

--- week12(New)/Assets/Scripts/RayShooter.cs
+++ week12(New)/Assets/Scripts/RayShooter.cs
@@ -11,45 +11,17 @@ public class RayShooter : MonoBehaviour
         _camera = GetComponent<Camera>();
     }
 
-    void OnGUI()
+    private void OnGUI()
     {
         int size = 12;
-        float posX = _camera.pixelWidth / 2 - size/4;
-        float posY = _camera.pixelHeight / 2 - size/2;  
-        GUI.Label(new Rect(posX, posY, size, size), "*","red");
+        float posX = _camera.pixelWidth / 2 - size / 4;
+        GUI.color = Color.red;
+        GUI.Label(new Rect(posX, posY, size, size), "*");
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            float posX = _camera.pixelWidth / 2;
-            float posY = _camera.pixelHeight / 2;
-            Vector3 point = new Vector3(posX, posY, 0);
-            Ray ray = _camera.ScreenPointToRay(point);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
-                GameObject hitObject = hit.transform.gameObject;
-                ReactiveTarget target = hitObject.GetComponent<ReactiveTarget>();
-                if (target != null)
-                {
-                    target.ReacToHit();
-                }
-                else
-                {
-                    StartCoroutine(ShpereIndicator(hit.point));
-                }
-            }
-        }
-    }
-
-    IEnumerator ShpereIndicator(Vector3 pos)
-    {
-        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        sphere.transform.position = pos;
-        yield return new WaitForSeconds(1);
-
-        Destroy(sphere);
+        
     }
 }
